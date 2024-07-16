e3fc21c79d59313eca5ab32861b2368367d6ee01
Mohammed Ibrahim Mahmoud <mohammedjrt@gmail.com>
2024-07-16 17:54:51 +0300

    adjust the page to work on tablets

--- src/pages/Technology.jsx
+++ src/pages/Technology.jsx
@@ -8,13 +8,13 @@ const Technology = () => {
   console.log(window.innerWidth);
   return (
     <main className='text-white mx-auto  container overflow-hidden p-6 sm:p-10 lg:px-[165px] relative  lg:py-[48px]'>
-      <div className='grid grid-cols-1 gap-6 '>
+      <div className='grid grid-cols-1  gap-6 '>
         <MainHeader
           number={"03"}
           label={"SPACE LAUNCH 101"}
         />
-        <div className='grid grid-rows-2 gap-8  relative '>
-          <div className=' overflow-y-clip  absolute h-[250px]'>
+        <div className='grid grid-rows-2 lg:grid-cols-2 gap-8 lg:grid-rows-none relative '>
+          <div className=' overflow-y-clip right-0 left-0 bottom-1/2 absolute h-[250px] sm:h-[300px]'>
             {/* add a black mask at the top and bottom */}
             <img
               src={
@@ -22,11 +22,11 @@ const Technology = () => {
                   ? technology[technologyIndex].images.portrait
                   : technology[technologyIndex].images.landscape
               }
-              className=' min-w-[100vw] absolute  inset-y-0 -left-6 sm:-translate-x-10 lg:-translate-x-[165px] object-bottom     '
+              className=' min-w-[100vw] absolute   -left-6 sm:-left-10  lg:-translate-x-[165px]  bottom-0 '
               alt=''
             />
           </div>
-          <div className='grid grid-cols-1 gap-10 row-span-'>
+          <div className='grid grid-cols-1 row-start-2 row-end-3 gap-10 row-span-'>
             <div className='flex justify-center gap-4'>
               <Circul
                 setTechnologyIndex={setTechnologyIndex}
@@ -44,8 +44,8 @@ const Technology = () => {
                 number={3}
               />
             </div>
-            <div className='w-full'>
-              <div className='flex flex-col   h-[245px] lg:h-full lg:grid '>
+            <div className='max-w-[500px] mx-auto'>
+              <div className='flex flex-col container  h-[245px] lg:h-full lg:grid '>
                 <div className='flex  flex-col lg:justify-end gap-4'>
                   <h3 className='font-Bellefair text-center  text-[18px]  opacity-50 sm:text-[24px] lg:text-[28px]  uppercase lg:text-left'>
                     the terminology
